7b93d09ac004318e8c9d97264db8f602c13fe40e
Scott Chou <scott.ph.chou@gmail.com>
2017-08-14 18:29:55 +0800

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,10 @@
+import PackageDescription
+
+let package = Package(
+    name: "Heimdallr",
+    dependencies : [
+        .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 1),
+        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3)
+    ],
+    exclude: ["Tests"]
+)
